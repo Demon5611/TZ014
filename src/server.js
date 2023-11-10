a7165394b54724b9c1897d60c56a687ad8b5d3eb
Demon5611 <d555601@gmail.com>
2023-11-10 20:35:05 +0300

    !

--- src/server.js
+++ src/server.js
@@ -1,8 +1,5 @@
 import express from 'express';
 import morgan from 'morgan';
-
-import session from 'express-session';
-import store from 'session-file-store';
 import path from 'path';
 import jsxRender from './utils/jsxRender';
 import indexRouter from './routes/indexRouter';
@@ -15,19 +12,6 @@ require('dotenv').config();
 
 const PORT = process.env.SERVER_PORT || 3000;
 const app = express();
-const FileStore = store(session);
-
-const sessionConfig = {
-  name: 'user_sid',
-  secret: process.env.SESSION_SECRET ?? 'test',
-  resave: true,
-  store: new FileStore(),
-  saveUninitialized: false,
-  cookie: {
-    maxAge: 1000 * 60 * 60 * 12,
-    httpOnly: true,
-  },
-};
 
 app.engine('jsx', jsxRender);
 app.set('view engine', 'jsx');
@@ -36,7 +20,6 @@ app.use(express.static('public'));
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(session(sessionConfig));
 app.use(resLocals);
 app.use(express.urlencoded({ extended: true }));
 
